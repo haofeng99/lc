4fb2673b167179ca3128af1e6f7468367a455e14
haofeng99 <haofengcheng@outlook.com>
2026-03-24 00:20:03 +0800

    测试git账号

---
+++ dynamic_programming/profit/maxProfit_VII.go
@@ -0,0 +1,6 @@
+package dynamicprogramming
+
+// 刚好交易K次
+func maxProfit_VII(prices []int, k int) int {
+	return 0
+}
---
+++ dynamic_programming/profit/maxProfit_VIII.go
@@ -0,0 +1,7 @@
+package dynamicprogramming
+
+// 最少交k次
+// 一个思路时将问题准化为刚好交易k次+无限次交易的子问题
+func maxProfit_VIII(prices []int, k int) int {
+	return 0
+}
